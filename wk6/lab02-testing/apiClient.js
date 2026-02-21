b77684bfa87e11649ff06ca0a269be0d396d9b48
jdetok <jdeko17@gmail.com>
2026-02-21 11:30:16 -0600

    calculator tests working

--- wk6/lab02-testing/apiClient.js
+++ wk6/lab02-testing/apiClient.js
@@ -1,14 +1,39 @@
 const URL = "https://jsonplaceholder.typicode.com/users";
 export async function fetchUsers() {
     try {
-        res = await fetch(URL)
-        console.log(`response status: ${res.status}`);
-
-        users = await res.json();
-        console.log(users);
+        return await fetch(URL).then((r) => r.json());
     } catch(e) { throw new Error(`failed to fetch users: ${e}`) }
 }
-async function fetchUserById(id) {}
-async function createUser(id) {}
-async function updateUser(id, user) {}
-async function deleteUser(id) {}
+export async function fetchUserById(id) {
+    try {
+        const user = await fetch(`${URL}/${id}`).then((r) => r.json());
+        return { id: user.id, name: user.name, email: user.email };
+    } catch(e) { throw new Error(`failed to fetch user with ID ${id}: ${e}`) }
+}
+export async function createUser() {
+    try {
+        return await fetch(URL, {
+            method: 'POST',
+            body: JSON.stringify({
+                name: 'Justin DeKock', username: 'jdeto', email: 'test'
+            }),
+            headers: { 'Content-Type': 'application/json: charset=UTF-8' }
+        }).then((r) => r.json())
+    } catch(e) { throw new Error(`failed to create user: ${e}`) }
+}
+export async function updateUser(id) {
+    try {
+        return await fetch(`${URL}/${id}`, {
+            method: 'PATCH',
+            body: JSON.stringify({
+                name: 'New Name', username: 'newname',
+            }),
+            headers: { 'Content-Type': 'application/json; charset=UTF-8' }
+        }).then((r) => r.json())
+    } catch(e) { throw new Error(`failed to update user with ID ${id}: ${e}`) }
+}
+export async function deleteUser(id) {
+    try {
+        return await fetch(`${URL}/${id}`, {method: 'DELETE'}).then((r) => r.status);
+    } catch(e) { throw new Error(`failed to delete user with ID ${id}: ${e}`) }
+}
--- wk6/lab02-testing/apiClient.test.js
+++ wk6/lab02-testing/apiClient.test.js
@@ -1,3 +1,29 @@
+import {fetchUsers, fetchUserById, createUser, updateUser, deleteUser } from "./apiClient.js"
+
 test("apiClient placeholder", () => {
-  expect(true).toBe(true);
+    expect(true).toBe(true);
+});
+
+const mockUsers = [
+    { id: 1, name: 'Mock 1', email: 'mock1@email.com'},
+    { id: 2, name: 'Mock 2', email: 'mock2@email.com'},
+];
+
+beforeEach(() => {
+    jest.spyOn(global, 'fetch').mockResolvedValue({
+        ok: true,
+        json: async () => mockUsers,
+    });
+});
+
+afterEach(() => {
+    jest.restoreAllMocks();
+});
+
+describe('fetchUsers', () => { 
+    test('returns array of users', async () => {
+        const users = await fetchUsers();
+        expect(users).toEqual(mockUsers);
+        expect(global.fetch).toHaveBeenCalledTimes(1);
+    }); 
 });
