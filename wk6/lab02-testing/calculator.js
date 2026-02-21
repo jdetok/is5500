31c5b4c19187bbec4adf0b7a355da5bfd89df389
jdetok <jdeko17@gmail.com>
2026-02-21 10:37:14 -0600

    calculator tests working

--- wk6/lab02-testing/calculator.js
+++ wk6/lab02-testing/calculator.js
@@ -1,15 +1,48 @@
+const numsErrMsg = (a, b) => `both values must be numbers or numeric strings: ${a} | ${b}`;
+const numErrMsg = (a) => `value must be a number or numeric string: ${a}`;
+const zeroNumErrMsg = (a) => `number must be greater than 0: ${a}`;
+
+// check for number or numeric string
+function isNumber(val) {
+    return typeof val !== "boolean" && val !== null && val !== "" && !Number.isNaN(Number(val));   
+}
+
 export function add(a, b) {
-    return  a + b;
+    if (!isNumber(a) || !isNumber(b)) {
+        throw new Error(numsErrMsg(a, b))
+    }
+    return a + b;
 }
 
 export function subtract(a, b) {
+    if (!isNumber(a) || !isNumber(b)) {
+        throw new Error(numsErrMsg(a, b))
+    }
     return a - b;
 }
 
 export function multiply(a, b) {
+    if (!isNumber(a) || !isNumber(b)) {
+        throw new Error(numsErrMsg(a, b))
+    }
     return a * b;
 }
 
 export function divide(a, b) {
+    if (!isNumber(a) || !isNumber(b)) {
+        throw new Error(numsErrMsg(a, b))
+    }
+    if (b === 0) { throw new Error(zeroNumErrMsg(b)) }
     return a / b;
+}
+
+export function square(a) {
+    if (!isNumber(a)) { throw new Error(numErrMsg(a)) }
+    return a * a;
+}
+
+export function squareRoot(a) {
+    if (!isNumber(a)) { throw new Error(numErrMsg(a)) }
+    if (a < 0) { throw new Error(zeroNumErrMsg(a)) }
+    return Math.sqrt(a);
 }
--- wk6/lab02-testing/calculator.test.js
+++ wk6/lab02-testing/calculator.test.js
@@ -1,14 +1,43 @@
-import { add, subtract, multiply, divide } from "./calculator.js"
+import { add, subtract, multiply, divide, square, squareRoot } from "./calculator.js"
 
-test('adds 1 + 2 to equal 3', () => {
-    expect(add(1, 2)).toBe(3);
+describe('basic passing calculator tests', () => {
+    test('adds 1 + 2 to equal 3', () => {
+        expect(add(1, 2)).toBe(3);
+    });
+    test('subtracts 3 from 2 to equal 1', () => {
+        expect(subtract(3, 2)).toBe(1);
+    });
+    test('multiplies 3 * 2 to equal 6', () => {
+        expect(multiply(3, 2)).toBe(6);
+    });
+    test('divides 6 by 2 to equal 3', () => {
+        expect(divide(6, 2)).toBe(3);
+    });
+    test('square 3 to be 9', () => {
+        expect(square(3)).toBe(9);
+    });
+    test('square root 9 to be 3', () => {
+        expect(squareRoot(9)).toBe(3);
+    });
 });
-test('subtracts 3 from 2 to equal 1', () => {
-    expect(subtract(3, 2)).toBe(1);
-});
-test('multiplies 3 * 2 to equal 6', () => {
-    expect(multiply(3, 2)).toBe(6);
-});
-test('divides 6 by 2 to equal 3', () => {
-    expect(divide(6, 2)).toBe(3);
+
+describe('test that non number values fail', () => {
+    test('add non number fail', () => {
+        expect(() => add("1", "h")).toThrow();
+    });
+    test('subtract non number fail', () => {
+        expect(() => subtract("1", "h")).toThrow();
+    });
+    test('multiply non number fail', () => {
+        expect(() => multiply("1", "h")).toThrow();
+    });
+    test('divide non number fail', () => {
+        expect(() => divide("1", "h")).toThrow();
+    });
+    test('square fail', () => {
+        expect(() => square("l")).toThrow();
+    });
+    test('square root fail', () => {
+        expect(() => squareRoot("l")).toThrow();
+    });
 });
